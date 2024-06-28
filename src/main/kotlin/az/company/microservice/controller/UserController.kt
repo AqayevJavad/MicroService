a82fd54521ef23c0ba27522c8664ac36049ad312
Javad Aghayev (Intern) <Javad.Aghayev@pashabank.az>
2024-06-28 15:13:00 +0400

    Update UserController.kt

--- src/main/kotlin/az/company/microservice/controller/UserController.kt
+++ src/main/kotlin/az/company/microservice/controller/UserController.kt
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
 class UserController(
     private val userService: UserService
 ) {
@@ -21,17 +21,17 @@ class UserController(
             ResponseEntity.notFound().build()
         }
     }
-    @PostMapping
+    @PostMapping("/adduser")
     fun createUser(@RequestBody user: User): ResponseEntity<User> {
         val savedUser = userService.saveUser(user)
         return ResponseEntity(savedUser, HttpStatus.CREATED)
     }
-    @GetMapping
+    @GetMapping("/users")
     fun getUsers(): ResponseEntity<List<User>> {
         val users = userService.getUsers()
         return ResponseEntity(users, HttpStatus.OK)
     }
-    @DeleteMapping
+    @DeleteMapping("/delete/{id}")
     fun deleteUser(@PathVariable id: Long): ResponseEntity<Void> {
         val existingUser=userService.getUserById(id)
             return if (existingUser!=null){
@@ -41,7 +41,7 @@ class UserController(
                 ResponseEntity.notFound().build()
             }
     }
-    @PutMapping("/{id}")
+    @PutMapping("/add/{id}")
     fun updateUserById(@PathVariable id: Long, @RequestBody updateUser: User): ResponseEntity<User> {
          val existingUser=userService.getUserById(id)
         return if (existingUser == null){
